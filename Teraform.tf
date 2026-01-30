45a95ff7d37162206cb2b22e888d794f4009dff5
shb007869-lgtm <shb007869@gmail.com>
2026-01-30 19:52:01 +0530

    Update and rename file1.txt~ to Teraform.tf

---
+++ Teraform.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "devops" {
+  count         = 5
+  ami           = "ami-0532be01f26a3de55"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
